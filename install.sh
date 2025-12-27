149dd5dbc64fc66c57b4ac6320a90e936e3f87f5
yutian929 <yutian929@outlook.com>
2025-12-27 21:55:25 +0800

    basic

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ROOT DIR
+# Install pybind11
+cd SDK/
+git clone https://github.com/pybind/pybind11.git && cd pybind11 && mkdir build && cd build && cmake .. && make && sudo make install
+cd ../../../
+
+# Build bimanual python bindings
+cd SDK/R5/py/ARX_R5_python/bimanual/
+mkdir -p build && cd build && cmake .. && make && make install
+cd ../../../../../../
