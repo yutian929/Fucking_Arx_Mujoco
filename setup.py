46ac83433efadd7845f93982c7f9e59fc8ab71e9
yutian929 <yutian929@outlook.com>
2026-01-10 12:26:45 +0800

    renew setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.1.1',                       # 版本号
     packages=find_packages(),             # 自动发现包
     install_requires=[                    # 包依赖
-        # 'numpy', 'torch', 'opencv-python',  # 示例依赖项
+        'numpy',
+        'scipy',
     ],
     include_package_data=True,             # 包含其他文件
     package_data={                         # 额外文件
